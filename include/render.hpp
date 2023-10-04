61a09f003706571668794f17c7062a335e516ec3
Chenjiefeng <1497735430@qq.com>
2023-10-04 20:53:41 +0800

    modified new structure

--- include/render.hpp
+++ include/render.hpp
@@ -1,7 +1,4 @@
 #ifndef F057F818_58D8_42C1_9212_9C2394136E13
 #define F057F818_58D8_42C1_9212_9C2394136E13
-#include "./mat_algo.h"
-#include "./error.h"
-// template <typename T>
 
 #endif /* F057F818_58D8_42C1_9212_9C2394136E13 */
--- src/client.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLIENT
-#define CLIENT
-#include "../Imgui/imgui.h"
-#include "../Imgui/backends/imgui_impl_win32.h"
-#include "../Imgui/backends/imgui_impl_dx11.h"
-
-#include "../include/render.h"
-#endif
-int client();
